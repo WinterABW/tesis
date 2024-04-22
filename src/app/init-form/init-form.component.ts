ec077943c52c5047146469afea0b10466682405e
victormat <skarnanotherblackwinter@gmail.com>
2024-04-22 17:04:23 -0400

    delete init form

--- src/app/init-form/init-form.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-init-form',
-  templateUrl: './init-form.component.html',
-  styleUrls: ['./init-form.component.scss']
-})
-export class InitFormComponent {
-}
